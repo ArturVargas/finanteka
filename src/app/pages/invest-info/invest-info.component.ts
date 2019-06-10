7f3732a598a10809e5560f1eb790d0d9d5aa6d28
Antonio Mendoza <antonio.mendoza.mmg@gmail.com>
2019-06-10 00:24:01 -0500

    Dashboard Design

--- src/app/pages/invest-info/invest-info.component.ts
+++ src/app/pages/invest-info/invest-info.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ChartDataSets, ChartOptions, ChartType } from 'chart.js';
+import { Label, Color } from 'ng2-charts';
 
 @Component({
   selector: 'app-invest-info',
@@ -7,9 +9,68 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InvestInfoComponent implements OnInit {
 
+  public ChartOptions: ChartOptions = {
+    responsive: true,
+    maintainAspectRatio: false,
+    // We use these empty structures as placeholders for dynamic theming.
+    scales: { xAxes: [{}], yAxes: [{}] },
+    plugins: {
+      datalabels: {
+        anchor: 'end',
+        align: 'end',
+      }
+    }
+  };
+  public ChartLabels: Label[] = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio'];
+  public ChartType: ChartType = 'line';
+  public ChartLegend = true;
+  public GradientFill;
+  public ChartColor: Color[] = [
+    {
+      borderWidth: 2,
+      backgroundColor: 'rgba(165, 162, 251, 0.3)',
+      borderColor: 'rgba(165, 162, 251, 1)'
+    },
+    {
+      borderWidth: 2,
+      backgroundColor: 'rgba(52, 226, 156, 0.3)',
+      borderColor: 'rgba(52, 226, 156, 1)'
+    }
+  ] 
+  public ChartData: ChartDataSets[] = [
+    { data: [], label: 'Dinero Disponible' },
+    { data: [], label: 'En Fondeo' }
+  ];
+
   constructor() { }
 
   ngOnInit() {
+    var ctx = document.getElementsByTagName('canvas')[0].getContext("2d");
+    this.GradientFill = ctx.createLinearGradient(0, 0, 0, 300);
+    this.GradientFill.addColorStop(0, "rgba( 165, 162, 251, 0.6)");
+    this.GradientFill.addColorStop(1, "rgba( 255, 255, 255, 0)");
+    this.ChartColor[0].backgroundColor = this.GradientFill;
+    // Color 2
+    this.GradientFill = ctx.createLinearGradient(0, 0, 0, 300);
+    this.GradientFill.addColorStop(0, "rgba( 52, 226, 156, 0.6)");
+    this.GradientFill.addColorStop(1, "rgba( 52, 226, 156, 0)");
+    this.ChartColor[1].backgroundColor = this.GradientFill;
+    this.calcular();
+  }
+
+  chartHovered({event, active }: {event: MouseEvent, active:{}[]}) {
+    console.log(event, active);
+  }
+
+  calcular() {
+    const data = [
+      1000,800,1000,1100,1000,1300
+    ];
+    const data2 = [
+      1100,700,1050,900,1100,750
+    ];
+    this.ChartData[0].data = data;
+    this.ChartData[1].data = data2;
   }
 
 }
