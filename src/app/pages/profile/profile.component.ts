b3e3f67ddf78b7a2fbca3aac8aef83383f1bb293
Antonio Mendoza <antonio.mendoza.mmg@gmail.com>
2019-06-11 17:30:29 -0500

    Perfil

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -12,6 +12,14 @@ export class ProfileComponent implements OnInit {
   constructor(public modal: MatDialog) { }
 
   ngOnInit() {
+    var profile = document.getElementById('profile');
+    var edit = document.getElementById('edit');
+    var activeEP = false;
+    edit.addEventListener('click', function (e) {
+      var attr = activeEP ? 'target compact' : 'target compact activeE';
+      profile.setAttribute('class', attr);
+      activeEP = activeEP ? false : true;
+    });
   }
   openMod() {
     const modalRef = this.modal.open(InvestFormComponent)
