d8e58cc9134d7c663e1c8444f8943ca97818de55
Antonio Mendoza <antonio.mendoza.mmg@gmail.com>
2019-06-11 14:46:24 -0500

    perfil

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -5,7 +5,7 @@ import { InvestFormComponent } from '../invest-form/invest-form.component';
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.scss']
+  styleUrls: ['./profile.component.scss','../../app.component.scss']
 })
 export class ProfileComponent implements OnInit {
 
